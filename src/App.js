84a86b209da15051767d402fba0f027243da3d12
SherondaMD <sheronda.mckee@gmail.com>
2023-09-01 17:13:07 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/dr-sheronda-mckee-dollar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sheronda McKee-Dollar
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/SherondaMD/smd-react-weather-app2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/dr-sheronda-mckee-dollar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sheronda McKee-Dollar
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/SherondaMD/smd-react-weather-app2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
