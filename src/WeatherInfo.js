aba0ed498935004afc041e7c16c8492a4ab3f3bb
SherondaMD <sheronda.mckee@gmail.com>
2023-09-02 14:25:01 -0700

    used custom api component and added animated icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate.js";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -10,21 +11,29 @@ export default function WeatherInfo(props) {
           <FormattedDate date={props.data.date} />
         </li>
         <li className="text-capitalize">{props.data.description}</li>
-        <li>{props.data.icon}</li>
-        <li>{props.data.temperature}</li>
       </ul>
 
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix"></div>
-        </div>
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+          </div>
 
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}</li>
-          </ul>
+          <div className="float-left">
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">F</span>
+          </div>
         </div>
+      </div>
+
+      <div className="col-6">
+        <ul>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind: {props.data.wind}</li>
+        </ul>
       </div>
     </div>
   );
